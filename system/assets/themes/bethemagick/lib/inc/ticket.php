54760ce3749cdbf0459094bf2cf627ef58ec76c2
CMO <catherine@msdlab.com>
2018-01-31 22:11:26 -0500

    get the medical form force in place.

--- system/assets/themes/bethemagick/lib/inc/ticket.php
+++ system/assets/themes/bethemagick/lib/inc/ticket.php
@@ -32,9 +32,15 @@ if(!class_exists('MSDLab_tickets')){
             add_action('yith_wcevti_custom_option',array(&$this,'add_select'),10,2);
             add_action('yith_wcevti_custom_field',array(&$this,'custom_field_frontend'),10,4);
 
+            //woocommerce
+            remove_all_actions('woocommerce_after_single_product_summary');
             add_filter('woocommerce_sale_flash',array(&$this,'edit_sale_text'),10,3);
             add_filter('loop_shop_columns', array(&$this,'loop_columns'),15);
+            add_action('woocommerce_before_add_to_cart_button', array(&$this,'medical_form_button'),99);
+            add_action('genesis_header', array(&$this,'medical_form_header'),99);
 
+            //gravity forms
+            add_action( 'gform_after_submission_4', array(&$this,'post_medical_form'), 10, 2 );
         }
         //methods
         //core
@@ -97,6 +103,55 @@ if(!class_exists('MSDLab_tickets')){
         // Change number or products per row to 3
         function loop_columns() {
             return 3; // 3 products per row
+        }
+
+        function medical_form_button(){
+            global $post;
+            if(has_term( 17, 'product_cat', $post )){
+                print '<div class="medical-form-notice">You must fill out a medical form with each registration.</div>';
+                print '<a class="button alt medical-form-trigger">Medical Release</a>';
+            }
+        }
+
+        function medical_form_header(){
+            if(is_single() && is_product()){
+                global $post;
+                if(has_term( 17, 'product_cat', $post )){
+                    //add javascript
+                    $script = "<script>
+jQuery(document).ready(function($) {	
+    $('.single_add_to_cart_button').attr('disabled','disabled');
+    $('.medical-form-trigger').click(function(){
+        $('#medical_modal').modal('show');
+    });
+  
+    $(document).bind('gform_confirmation_loaded', function(event, formId){
+        // code to be trigger when confirmation page is loaded
+        $('.medical-form-notice').html('Thank you for submitting the medical release. You may now add this registration to your cart.');
+        $('#medical_modal').modal('hide');
+        $('.single_add_to_cart_button').removeAttr('disabled');
+    });
+});
+</script>";
+                    print $script;
+                    //add modal with form (ajax)
+                    $modal = '
+        <div class="modal fade" tabindex="-1" role="dialog" id="medical_modal">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        '.do_shortcode('[gravityform id="4" title="true" description="true" ajax="true"]').'
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->';
+                    print $modal;
+                }
+            }
+        }
+
+        function post_medical_form($entry, $form){
+
         }
         //util
     }
