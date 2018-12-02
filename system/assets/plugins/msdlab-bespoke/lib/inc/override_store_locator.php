7c8f212bca31d658580d82e7473c36b837b066b4
CMO <catherine@msdlab.com>
2018-12-02 15:27:04 -0500

    overrides for store locator

--- system/assets/plugins/msdlab-bespoke/lib/inc/override_store_locator.php
+++ system/assets/plugins/msdlab-bespoke/lib/inc/override_store_locator.php
@@ -6,3 +6,35 @@ function bfc_meta_box_fields( $meta_fields ) {
     unset($meta_fields['Location']['address2']);
     return $meta_fields;
 }
+add_filter ( 'wpsl_info_window_template', 'bfc_obfuscate_email', 20);
+add_filter ( 'wpsl_listing_template', 'bfc_obfuscate_email', 20);
+
+function bfc_obfuscate_email($template){
+    $pattern = '/<span><strong>Email<\/strong>: (.*?)<\/span>/m';
+    $replace = '<span><strong>Contact</strong>: <a href="/contact?coven_id=<%= id %>"><%= store %></a></span>';
+    $template = preg_replace($pattern,$replace,$template);
+
+    return $template;
+}
+
+add_filter( 'gform_field_value_recipient_email', 'bfc_get_recipient_email');
+
+function bfc_get_recipient_email(){
+    if($coven_id = $_GET['coven_id']){
+        $email = get_post_meta($coven_id,'wpsl_email',true);
+        return $email;
+    } else {
+        return 'kate@msdlab.com';
+    }
+}
+
+add_filter( 'gform_field_value_recipient_name', 'bfc_get_recipient_name');
+
+function bfc_get_recipient_name(){
+    if($coven_id = $_GET['coven_id']){
+        $title = get_the_title($coven_id);
+        return $title;
+    } else {
+        return 'Seeker Coordinator';
+    }
+}
